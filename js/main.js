b24b55da40db4f141389eef6f356de9af27c0835
abner.albino <abner.albino@gmail.com>
2022-07-02 22:00:25 -0300

    Desafio 1

---
+++ js/main.js
@@ -0,0 +1,93 @@
+let tituloDeLaPosicion = prompt("Título de la Vancante");
+let id = parseInt(prompt("Entre el ID del puesto"));
+let vacantes = parseInt(prompt("Cuantas vacantes hay que llenar para este puesto?"));
+
+alert("El puesto " + tituloDeLaPosicion + " ID " + id + " se ha registrado exitosamente y hay " + vacantes + " para llenar.")
+
+let vacantesLlenas = parseInt(prompt("Cuántas de las vacantes se ha llenado?"));
+let vacantesRestantes = vacantes - vacantesLlenas;
+
+let razonDeLlenado = (100 * vacantesLlenas) / vacantes;
+
+if (vacantesLlenas > vacantes) {
+    alert("Número de vacantes llenas debe ser menor o igual al número de vacantes");
+
+    
+}
+else if (vacantesLlenas == vacantes) {
+    alert ("Ya llenaste todas las vacantes. Podés cerrar el puesto");
+    
+}
+
+else if (vacantesLlenas < vacantes) {
+    alert ("Has llenado el" + razonDeLlenado + "% de las vacantes. Te quedan " + vacantesRestantes + " por llenar");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let tituloDeLaPosicion = prompt("Título de la Vancante");
+
+let id = prompt(parseInt("Entre el ID de la cavante"));
+
+let inicio = prompt (new Date("Cuándo va a ser el primer dia de  trabajo del contratado"));
+
+let limite = new Date("2022/07/29");
+/* let hoy = new date("");  
+
+let vacantes = prompt("Cuantas vacantes hay que llenar para este puesto?");
+
+if (vacantes <=0) {
+    alert("El puesto tiene que tener por lo menos una vancate");
+    
+}
+
+else if (asignacion) {
+
+}
+
+let asignacion = prompt("Cuándo te asignaron la búsqueda? DD/MM/YYYY");
+
+let fechaAsignacion = new Date(asignacion);
+
+let contratado = new Date("2022/06/29");
+
+let invitaciones = prompt("Cuántos candidatos has invitado a que se postulen?");
+
+let presentaciones = prompt("Cuántos candidatos has presnetado al cliente?");
+
+let entrevistas = prompt("Cuántos candidatos fueron entrevistados por el cliente?");
+
+let contratados = prompt("Cuántos candidatos fueron contratados por el cliente?");
+
+
+
+let tiempoParaPresentarSeg = limite - asignacion;
+let diasParaPresentar = tiempoParaPresentarSeg / (1000 * 3600 * 24);
+
+
+alert(`El puesto ${tituloDeLaPosicion} (ID  ${id}) se ha registrado exitosamente. 
+Tenés ${diasParaPresentar} días para presentar los candidatos de la vacante`); */
