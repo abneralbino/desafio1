7422e8f414f054022830eb02b37b24fa737988e6
abner.albino <abner.albino@gmail.com>
2022-07-07 00:35:06 -0300

    intentando hacer prompt con fecha dd/mm/aaaa

--- js/main.js
+++ js/main.js
@@ -1,8 +1,64 @@
+/* IDENTIFICACIÓN DEL PUESTO
 let tituloDeLaPosicion = prompt("Título de la Vancante");
 let id = parseInt(prompt("Entre el ID del puesto"));
-let vacantes = parseInt(prompt("Cuantas vacantes hay que llenar para este puesto?"));
+let nombreCliente = prompt("Entre el nombre del cliente");
+let vacantes = parseInt(prompt("Cuántas vacantes hay que llenar para este puesto?"));
+let fechaInicio = Date.parse(prompt("Fecha de inicio del puesto")); /*conseguir prompt para fecha*/
+/*
+let fechaLimite = Date /*conseguir prompt para fecha. La fecha límite es 5 días antes de fechaInicio*/
+/*
+alert("El puesto " + tituloDeLaPosicion + " ID " + id + " para " + nombreCliente + " se ha registrado exitosamente y hay " + vacantes + " para llenar.")
 
-alert("El puesto " + tituloDeLaPosicion + " ID " + id + " se ha registrado exitosamente y hay " + vacantes + " para llenar.")
+/*METRICAS DEL RECLUTADOR
+let invitaciones = parseInt(prompt("Cuántos candidatos fueron invitados a postularse?"));
+let invitacionesAceptadas = parseInt(prompt("Cuántos candidatos acceptaron las invitaciones y se postularon?"));
+let presentaciones = parseInt(prompt("Cuántos candidatos fueron presentados al cliente?"));
+let fechaPresentacion = Date /*conseguir prompt para fecha
+let entrevistas = parseInt(prompt("Cuántos candidatos fueron entrevistados por el cliente?"));
+let cantContrataciones = parseInt(prompt("Cuántos candidatos fueron contratados por el cliente?"))
+let fechaCierrePuesto = Date /*conseguir prompt para fecha*/
+
+
+/* functions para transformar fechas*/
+let fecha = prompt("Entre la fecha dd/mm/aaaa");
+
+function formatoDeFecha(fecha) {
+    nuevoFormato = fecha.toLocaleDateString('es-AR');
+    return new Date(fecha);
+} 
+
+
+
+/* function formatoDeFecha(fecha) {
+    nuevoFormato = toLocaleDateString('es-AR').parseInt(fecha);
+
+    return new Date(fecha);
+} 
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 let vacantesLlenas = parseInt(prompt("Cuántas de las vacantes se ha llenado?"));
 let vacantesRestantes = vacantes - vacantesLlenas;
@@ -23,7 +79,7 @@ else if (vacantesLlenas < vacantes) {
     alert ("Has llenado el" + razonDeLlenado + "% de las vacantes. Te quedan " + vacantesRestantes + " por llenar");
 
 }
-
+*/
 
 
 
